bc2e3d6d45d02eca15cbcd8e857e389a8b091d5c
Kunal Singh <127078613+KunalSingh5431@users.noreply.github.com>
2024-05-19 21:01:48 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,19 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { ChakraProvider } from '@chakra-ui/react';
+import ReactDOM from 'react-dom/client';
+import { ChakraProvider } from '@chakra-ui/react'
 import App from './App';
-import { BrowserRouter } from "react-router-dom";
+import {BrowserRouter} from "react-router-dom"
 import ChatProvider from './Context/ChatProvider';
 
-ReactDOM.render(
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <ChakraProvider>
-        <ChatProvider>
-          <App />
-        </ChatProvider>
-      </ChakraProvider>
+    <ChakraProvider>
+    <ChatProvider>
+    <App />
+    </ChatProvider>
+    </ChakraProvider>
     </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
+
